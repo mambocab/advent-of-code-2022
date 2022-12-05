d4ba21133eefa21835784d439bd2f5f24dc128a9
Jim Witschey <jim.witschey@gmail.com>
2022-12-05 12:29:15 -0500

    day 5 implementation

--- day5/main.go
+++ day5/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 	"text/tabwriter"
@@ -23,17 +24,77 @@ func main() {
 		}
 	}
 
-	crateStackSpec, _ := lines[:emptyLineIdx], lines[emptyLineIdx:]
+	crateStackSpec, movementsSpec := lines[:emptyLineIdx], lines[emptyLineIdx:]
 
-	parseCrateStackSpec(crateStackSpec)
+	p1Crates := parseCrateStackSpec(crateStackSpec)
+	p2Crates := [10]*crate{}
+	for i, c := range p1Crates {
+		if c != nil {
+			p2Crates[i] = c.clone()
+		}
+	}
+	moves := parseMovementSpec(movementsSpec)
 
-	// fmt.Println(crateStackSpec)
-	// fmt.Println()
-	// fmt.Println(movementSpec)
+	for _, m := range moves {
+		for i := 0; i < m.n; i++ {
+			if v, err := p1Crates[m.from].pop(); err != nil {
+				panic(err)
+			} else {
+				p1Crates[m.to].push(v)
+			}
+		}
+		if chopt, err := p2Crates[m.from].chop(m.n); err != nil {
+			panic(err)
+		} else {
+			p2Crates[m.to].plop(chopt...)
+		}
+	}
+
+	fmt.Print("part 1: ")
+	for _, c := range p1Crates[1:] {
+		fmt.Printf("%c", *c.peek())
+	}
+	fmt.Println()
+	fmt.Print("part 2: ")
+	for _, c := range p2Crates[1:] {
+		fmt.Printf("%c", *c.peek())
+	}
+	fmt.Println()
 }
 
 // assume all ASCII -- bytes not runes
 type crate = stack[byte]
+
+type move struct{ n, from, to int }
+
+var MOVE_REGEX = regexp.MustCompile(`\d+`)
+
+func atoiOrPanic(s string) (i int) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func parseMovementSpec(mss []string) (moves []move) {
+	for _, line := range mss {
+		if line == "" {
+			continue
+		}
+		matches := MOVE_REGEX.FindAllString(line, 4) // 4 helps us check for bad lines
+		if len(matches) != 3 {
+			panic("bad line")
+		}
+		// matches[0], matches[1], matches[2]
+		moves = append(moves, move{
+			n:    atoiOrPanic(matches[0]),
+			from: atoiOrPanic(matches[1]),
+			to:   atoiOrPanic(matches[2]),
+		})
+	}
+	return
+}
 
 func parseCrateStackSpec(css []string) [10]*crate {
 	rv := [10]*crate{
@@ -102,6 +163,10 @@ func (s *stack[T]) push(t T) {
 	s.vs = append(s.vs, t)
 }
 
+func (s *stack[T]) plop(ts ...T) {
+	s.vs = append(s.vs, ts...)
+}
+
 func (s *stack[T]) pop() (T, error) {
 	if len(s.vs) == 0 {
 		return *new(T), fmt.Errorf("stack is empty")
@@ -112,8 +177,35 @@ func (s *stack[T]) pop() (T, error) {
 	return rv, nil
 }
 
+func (s *stack[T]) chop(n int) ([]T, error) {
+	if len(s.vs) < n {
+		return nil, fmt.Errorf("stack too short to chop(%d): %v", n, s)
+	}
+	i := len(s.vs) - n
+	newVs, rv := s.vs[:i], s.vs[i:]
+	if len(rv) != n {
+		panic("oh no")
+	}
+	s.vs = newVs
+	return rv, nil
+}
+
+func (s *stack[T]) peek() *T {
+	if s.empty() {
+		return nil
+	}
+	return &s.vs[len(s.vs)-1]
+}
+
 func (s *stack[T]) empty() bool {
 	return len(s.vs) == 0
+}
+
+func (s *stack[T]) clone() *stack[T] {
+	rv := newStack[T]()
+	rv.vs = make([]T, len(s.vs))
+	copy(rv.vs, s.vs)
+	return rv
 }
 
 func (s *stack[T]) String() string {

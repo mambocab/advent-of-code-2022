b5db45de622dbd50b6d17e94a027c34944eff757
Jim Witschey <jim.witschey@gmail.com>
2022-12-15 22:32:37 -0500

    day 9 part 2

--- day9/main.py
+++ day9/main.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from functools import cache
 import operator as op
-from itertools import accumulate, chain
-from typing import Dict, Iterable, Tuple
+from itertools import accumulate, chain, tee
+from typing import Iterable, Optional, Tuple
 
 
 class Direction(Enum):
@@ -47,6 +47,18 @@ class Point:
                 return self.right
         raise ValueError("invalid direction", direction)
 
+    def drag(self, other: "Point") -> "Point":
+        """If self moves, and other is behind it, what position will other end up in?"""
+        if self.touching(other):
+            return other
+        if self.x == other.x or self.y == other.y:
+            if n := next(filter(self.touching, other.cardinal_adjacents)):
+                return n
+            raise ValueError("could not find new tail")
+        if n := next(filter(self.touching, other.diagonal_adjacents)):
+            return n
+        raise ValueError("could not find new tail")
+
     def touching(self, other: "Point") -> bool:
         return abs(self.x - other.x) <= 1 and abs(self.y - other.y) <= 1
 
@@ -64,40 +76,42 @@ class Point:
         yield self.down.left
         yield self.left.up
 
-    @property
-    def adjacents(self) -> Iterable["Point"]:
-        yield self.up
-        yield self.up.right
-        yield self.right
-        yield self.right.down
-        yield self.down
-        yield self.down.left
-        yield self.left
-        yield self.left.up
 
-
-@dataclass(frozen=True)
+@dataclass
 class State:
-    head: Point = Point(0, 0)
-    tail: Point = Point(0, 0)
+    knots: Tuple[Point] = None
+
+    def __init__(
+        self, length: Optional[int] = None, knots: Optional[Tuple[Point]] = None
+    ):
+        if length is not None and knots is not None:
+            raise ValueError("length and knots arguments are mutually exclusive")
+        if knots is not None:
+            self.knots = knots
+        else:
+            self.knots = tuple(Point() for _ in range(length))
 
     def move(self, direction: str) -> "State":
-        t = self.tail
-        h = self.head.move(direction=direction)
-        if h.touching(t):
-            return State(head=h, tail=t)
-        if h.x == t.x or h.y == t.y:
-            if new_tail := next(filter(h.touching, t.cardinal_adjacents)):
-                return State(head=h, tail=new_tail)
-            raise ValueError("could not find new tail")
-        if new_tail := next(filter(h.touching, t.diagonal_adjacents)):
-            return State(head=h, tail=new_tail)
-        raise ValueError("could not find new tail")
+        if not self.knots:
+            return State(0)
+        h = self.knots[0].move(direction)
+        return State(
+            knots=tuple(accumulate(self.knots[1:], func=Point.drag, initial=h))
+        )
+
+    @property
+    def tail(self):
+        return self.knots[-1]
 
 
+# fh = open("input.txt")
 fh = open("input.txt")
 
 split_lines = map(op.methodcaller("split"), map(op.methodcaller("strip"), fh))
 flattened_cmds = chain.from_iterable(map(lambda s: s[0] * int(s[1]), split_lines))
-states = accumulate(flattened_cmds, func=State.move, initial=State())
-print("part 1:", len(set(map(op.attrgetter("tail"), states))))
+p1_cmds, p2_cmds = tee(flattened_cmds)
+
+part1_states = accumulate(p1_cmds, func=State.move, initial=State(2))
+print("part 1:", len(set(map(op.attrgetter("tail"), part1_states))))
+part2_states = accumulate(p2_cmds, func=State.move, initial=State(10))
+print("part 2:", len(set(map(op.attrgetter("tail"), part2_states))))

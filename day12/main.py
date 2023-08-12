a440e27c0fb649110bc4a2f2ac128cf0df9af1b2
Jim Witschey <jim.witschey@gmail.com>
2023-08-12 10:32:28 -0400

    Stolen solution
    https://www.reddit.com/r/adventofcode/comments/zjnruc/2022_day_12_solutions/izw63we

--- day12/main.py
+++ day12/main.py
@@ -1,256 +1,35 @@
-from ast import Tuple
-from dataclasses import dataclass, field
-from enum import Enum, auto
-from functools import lru_cache
-from itertools import tee
-from time import sleep
-from typing import Iterable, Iterator, List, Optional
+input_lines = list(open("input.txt").readlines())
+input_grid = "".join(input_lines)
+coords = lambda i: complex(*divmod(i, len(input_lines[0])))
+height = lambda c: ord(grid.get(c, "Z").replace("S", "a").replace("E", "z"))
+source = coords(input_grid.index("E"))
+grid = {coords(x): input_grid[x] for x in range(len(input_grid))}
+
+# lines_in = open("example.txt").readlines()
+# grid_in = "".join(lines_in)
 
 
-class CellType(Enum):
-    """Represents the type of a cell in the grid"""
-
-    NORMAL = auto()
-    START = auto()
-    END = auto()
-
-    @classmethod
-    def from_string(cls, s: str) -> "CellType":
-        if len(s) > 1:
-            raise ValueError("string must be one character long")
-
-        match s:
-            case "S":
-                return cls.START
-            case "E":
-                return cls.END
-        return cls.NORMAL
+# def coords(i: int) -> complex:
+#     return complex(*divmod(i, len(lines_in[0])))
 
 
-class Direction(Enum):
-    """Represents a direction in the grid"""
-
-    RIGHT = auto()
-    DOWN = auto()
-    LEFT = auto()
-    UP = auto()
+# def height(c: complex) -> int:
+#     return ord(grid.get(c, "Z").replace("S", "a"))
 
 
-_lowercase_a = ord("a")
+# source = coords(grid_in.index("E"))
+# grid = {coords(x): grid_in[x] for x in range(len(grid_in))}
 
+for target in "S", "Sa":
+    print("target =", target)
+    todo, done = [(source, 0)], {source}
 
-@dataclass(frozen=True)
-class Cell:
-    elevation: int
-    type: CellType = CellType.NORMAL
-
-    @classmethod
-    def from_string(cls, s: str) -> "Cell":
-        if len(s) > 1:
-            raise ValueError("string must be one character long")
-
-        match s:
-            case "S":
-                elevation = 0
-            case "E":
-                elevation = ord("z") - _lowercase_a
-            case _:
-                if not s.islower():
-                    raise ValueError("invalid character")
-                elevation = ord(s) - _lowercase_a
-
-        return Cell(
-            elevation=elevation,
-            type=CellType.from_string(s),
-        )
-
-    def __str__(self) -> str:
-        if self.type == CellType.START:
-            return "S"
-        elif self.type == CellType.END:
-            return "E"
-        return chr(self.elevation + _lowercase_a)
-
-
-Point = tuple[int, int]
-Solution = tuple[Point, ...]
-
-
-@dataclass
-class SolutionGraphNode:
-    point: Point
-    parent: Optional["SolutionGraphNode"] = None
-    children: list["SolutionGraphNode"] = field(default_factory=list)
-
-    def add_child(self, child: "SolutionGraphNode") -> None:
-        self.children.append(child)
-        child.parent = self
-
-
-@dataclass
-class SolutionBuilder:
-    grid: "Grid"
-
-    _iter_count: int = 0
-
-    def build_solutions(self) -> Iterator[Solution]:
-        self._iter_count = 0
-        root = SolutionGraphNode(point=self.grid.start, parent=None)
-        yield from self._build_solutions((root,))
-
-    def _build_solutions(self, nodes: Iterable[SolutionGraphNode]) -> Iterator[Solution]:
-        self._iter_count += 1
-        nodes = list(nodes)
-        if self._iter_count % 1 == 0:
-            print(f"iter count: {self._iter_count}; nodes: {len(nodes)}")
-        next_nodes: list[SolutionGraphNode] = []
-        for node in nodes:
-            if node.point == self.grid.end:
-                yield self._solution_from_node(node)
-            else:
-                next_nodes.extend(
-                    SolutionGraphNode(point=adjacent, parent=node) for adjacent in self.grid.adjacents(node.point)
-                )
-
-        if next_nodes:
-            yield from self._build_solutions(tuple(next_nodes))
-
-    def _solution_from_node(self, node: Optional[SolutionGraphNode]) -> Solution:
-        solution: list[Point] = []
-        while node is not None:
-            solution.append(node.point)
-            node = node.parent
-        return tuple(reversed(solution))
-
-
-def solution_to_string(solution: Solution) -> str:
-    max_x, max_y = 0, 0
-    for x, y in solution:
-        max_x = max(max_x, x)
-        max_y = max(max_y, y)
-
-    grid_builder: list[list[str]] = [["."] * (max_x + 1) for _ in range(max_y + 1)]
-
-    p, next_p = tee(solution)
-    next(next_p)
-    for (x, y), (next_x, next_y) in zip(p, next_p):
-        match (next_x - x, next_y - y):
-            case (1, 0):
-                grid_builder[y][x] = ">"
-            case (-1, 0):
-                grid_builder[y][x] = "<"
-            case (0, 1):
-                grid_builder[y][x] = "v"
-            case (0, -1):
-                grid_builder[y][x] = "^"
-            case _:
-                ValueError("invalid solution")
-    grid_builder[next_y][next_x] = "E"
-
-    return "\n".join("".join(row) for row in grid_builder)
-
-
-@dataclass
-class Grid:
-    cells: tuple[tuple[Cell, ...], ...]
-    width: int
-    height: int
-    start: Point
-    end: Point
-
-    _iterations: int = 0
-
-    @classmethod
-    def from_string(cls, s: str) -> "Grid":
-        lines = s.splitlines()
-        height = len(lines)
-        width = len(lines[0])
-        print(f"width, height: {width}, {height}")
-
-        grid_builder: list[tuple[Cell, ...]] = []
-        start: Optional[Point] = None
-        end: Optional[Point] = None
-
-        line: str
-        for line_idx, line in enumerate(lines):
-            if line and (len(line) != width):
-                raise ValueError("all lines must be the same length")
-
-            line_builder = []
-
-            for cell_idx, c in enumerate(line):
-                if not c.isalpha():
-                    raise ValueError("all characters must be alphabetic")
-
-                cell = Cell.from_string(c)
-                line_builder.append(cell)
-                if cell.type == CellType.START:
-                    start = (cell_idx, line_idx)
-                elif cell.type == CellType.END:
-                    end = (cell_idx, line_idx)
-
-            grid_builder.append(tuple(line_builder))
-
-        if start is None:
-            raise ValueError("no start cell found")
-        if end is None:
-            raise ValueError("no end cell found")
-
-        return cls(
-            cells=tuple(grid_builder),
-            width=width,
-            height=height,
-            start=start,
-            end=end,
-        )
-
-    def __str__(self) -> str:
-        return "\n".join("".join(str(c) for c in row) for row in self.cells)
-
-    def __hash__(self) -> int:
-        return hash(self.cells)
-
-    @lru_cache
-    def adjacents(self, p: Point) -> frozenset[Point]:
-        x, y = p
-
-        rv = frozenset(
-            (x + dx, y + dy)
-            for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0))
-            if (0 <= x + dx < self.width) and (0 <= y + dy < self.height)
-        )
-        return rv
-
-    @lru_cache
-    def valid_moves(self, p: Point) -> frozenset[Point]:
-        return frozenset(self._valid_moves_generator(p))
-
-    def _valid_moves_generator(self, p: Point) -> Iterator[Point]:
-        px, py = p
-        max_elevation = self.cells[py][px].elevation + 1
-        for adj in self.adjacents(p):
-            if self.cells[adj[1]][adj[0]].elevation <= max_elevation:
-                yield adj
-
-    def solutions(self) -> Iterator[Solution]:
-        builder = SolutionBuilder(grid=self)
-        yield from builder.build_solutions()
-
-
-for filename in ("example.txt", "input.txt"):
-    # for now just the one
-    if filename.startswith("example"):
-        continue
-
-    print(filename)
-    print()
-
-    with open(filename) as fh:
-        grid = Grid.from_string(fh.read())
-
-    print(str(grid))
-
-    solution = next(grid.solutions())
-    print(len(solution) - 1, ":", solution)
-
-    print(solution_to_string(solution))
+    while todo:
+        old, dist = todo.pop(0)
+        if grid[old] in target:
+            print(dist)
+            break
+        for new in (old + 1, old - 1, old + 1j, old - 1j):
+            if new not in done and height(old) - height(new) <= 1:
+                todo.append((new, dist + 1))
+                done.add(new)
